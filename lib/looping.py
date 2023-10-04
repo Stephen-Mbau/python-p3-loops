5027adcadd218b7e3002176a096a025a4d9309ba
Stephen Mbau <mbaustephen@gmail.com>
2023-10-04 09:35:56 +0300

    changes

--- lib/looping.py
+++ lib/looping.py
@@ -3,10 +3,9 @@
 def happy_new_year():
     counter = 10
     while counter > 0:
-    print(counter)
-    counter -= 1
+        print(counter)
+        counter -= 1
     print("Happy New Year!")
-    happy_new_year ()
 
 def square_integers(int_list):
     squared_list = [num ** 2 for num in int_list]
@@ -14,11 +13,11 @@ def square_integers(int_list):
 
 def fizzbuzz():
     for num in range(1, 101):
-    if num % 3 == 0 and num % 5 == 0:
-    print("FizzBuzz")
-    elif num % 3 == 0:
-    print("Fizz")
-    elif num % 5 == 0:
-    print("Buzz")
-    else:
-    print(num)
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
